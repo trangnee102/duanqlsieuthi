dc3e994269fa4f75f7315c60b234535706161980
Nguyen Tien Anh <tienanhnguyen912@gmail.com>
2026-04-16 20:38:09 +0700

    fix bug 16/4

--- include/models/logic/TransactionModel.h
+++ include/models/logic/TransactionModel.h
@@ -5,7 +5,6 @@
 #include <string>
 #include <vector>
 #include "models/entities/Product.h"
-#include "models/entities/Transaction.h" // <== BẮT BUỘC PHẢI CÓ DÒNG NÀY
 
 /**
  * @brief Cấu trúc lưu thông tin món hàng trong giỏ hàng
@@ -19,20 +18,11 @@ struct CartItem {
 
 class TransactionModel {
 private:
-    std::vector<Transaction> transactionList; // <== CÁI KÉT ĐỰNG DỮ LIỆU ĐÂY
     std::string dataPath = "data/history_transactions.csv"; // Đường dẫn file của sếp
 
 public:
-    // Constructor: Tự động nạp dữ liệu khi sếp gọi TransactionModel reportModel;
-    TransactionModel() { loadTransactions(); }
-
-    // Hàm lấy danh sách để Case 11 dùng
-    std::vector<Transaction> getTransactions() const { return transactionList; }
-
-    /**
-     * @brief Nạp dữ liệu từ file CSV lên RAM
-     */
-    void loadTransactions();
+    // Constructor
+    TransactionModel() {}
 
     /**
      * @brief Thực hiện lưu hóa đơn xuống file
